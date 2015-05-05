ff681b2b6067f5166c1ce14914a238f234cbda21
Adam Seidman <adamjseidman@gmail.com>
2015-05-05 16:27:29 -0400

    Modified Potentiometer
    Fixed it so you can designate spots to get better values that are more
    custom to your needs.

--- src/org/usfirst/frc/team25/robot/Potentiometer.java
+++ src/org/usfirst/frc/team25/robot/Potentiometer.java
@@ -6,6 +6,7 @@ public class Potentiometer extends AnalogPotentiometer {
 
 	public final boolean inverted;
 	public double minimum = 0.0, maximum = 5.0;
+	public double minSpot = 0.0, maxSpot = 5.0;
 	
 	public Potentiometer(int channel) {
 		super(channel);
@@ -26,8 +27,30 @@ public class Potentiometer extends AnalogPotentiometer {
 			maximum = minimum;
 			minimum = temp;
 		}
+		if(minSpot != 0.0 || maxSpot != 5.0)
+			limitFixes();
 		val = ((val / 5.0) * (maximum - minimum)) + minimum;
 		return val;
+	}
+	
+	public void limitFixes() {
+		if(minSpot > maxSpot) {
+			double temp = minSpot;
+			minSpot = maxSpot;
+			maxSpot = temp;
+		}
+		if(maxSpot > 5.0)
+			maxSpot = 5.0;
+		if(minSpot < 0.0)
+			minSpot = 0.0;
+		double rangeFor1 = (maximum - minimum) / (maxSpot - minSpot);
+		minimum -= (rangeFor1 * minSpot);
+		maximum += (5.0 - maxSpot) * rangeFor1;
+		minSpot = 0.0;
+		maxSpot = 5.0;
+		System.out.println("1-range: " + rangeFor1);
+		System.out.println("min: " + minimum);
+		System.out.println("max: " + maximum);
 	}
 	
 }
